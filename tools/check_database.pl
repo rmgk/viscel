dd7d357cc02d52db7ed0ff645ba43190fb5bbad7
ragnar <ragnar@rmgk.de>
2009-07-19 17:55:06 +0200

    added some stats tracking to check_database.pl
    you can now test for regresssions with check_database.pl

--- tools/check_database.pl
+++ tools/check_database.pl
@@ -10,11 +10,24 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=../comics.db","","",{AutoCommit => 0,P
 
 my $ok = 0;
 my $all = 0;
-my @comics = scalar(@ARGV) ? @ARGV : sort keys %$comics;
+my $full_test = 1;
+my @comics;
+if (scalar(@ARGV)) {
+	@comics = @ARGV;
+	$full_test = 0;
+}
+else {
+	@comics = sort keys %$comics;
+}
+
+my %results;
+
 comic: foreach my $comic (@comics) {
 	next if !$comics->{$comic} or $comics->{$comic}->{broken};
 	#say $comic;
 	$all++;
+	$results{$comic}->{result} = 'undef';
+	$results{$comic}->{count} = 0;
 	my ($first,$last) = $dbh->selectrow_array("SELECT first,last FROM comics WHERE comic = ?",undef,$comic);
 	if (!$first or !$last) {
 		say "$comic: first or last missing";
@@ -23,40 +36,94 @@ comic: foreach my $comic (@comics) {
 	my $strips = $dbh->selectall_hashref("SELECT * FROM _$comic","id");
 	my $curr = $first;
 	my %seen;
+
 	do {
+		$results{$comic}->{count}++;
 		if ($seen{$curr}) {
 			say "$comic: $curr was already seen (circular reference)";
+			$results{$comic}->{result} ="seen$curr";
 			next comic;
 		}
 		$seen{$curr} = 1;
 		if (!$strips->{$curr}) {
 			say "$comic: $curr does not exist";
+			$results{$comic}->{result} ="notexist$curr";
 			next comic;
 		}
 		if ($curr == $last) {
 			$ok ++;
+			$results{$comic}->{result} ='ok';
 			next comic;
 		}
 		if (!$strips->{$curr}->{next}) {
 			say "$comic: $curr has no next";
+			$results{$comic}->{result} ="notnext$curr";
 			next comic;
 		}
 		else {
 			my $next = $strips->{$curr}->{next};
 			if ($next == $curr) {
 				say "$comic: $curr has itself ($next) as next";
+				$results{$comic}->{result} ="selfnext$curr";
 				next comic;
 			}
 			if (!$strips->{$next}->{prev}) {
 				say "$comic: next $next has no prev";
+				$results{$comic}->{result} ="notprev$next";
 				next comic;
 			}
 			if ($strips->{$next}->{prev} != $curr) {
 				say "$comic: next $next does not link back to $curr";
+				$results{$comic}->{result} ="next${next}notback$curr";
 				next comic;
 			}
 		}
 	} while ($curr = $strips->{$curr}->{next});
 }
+say "\n";
+$dbh->disconnect;
+if ($full_test) {
+	$dbh = DBI->connect("dbi:SQLite:dbname=check_database.db","","",{AutoCommit => 0,PrintError => 1});
+	$dbh->do('CREATE TABLE IF NOT EXISTS results(id INTEGER PRIMARY KEY ASC, count, ok, nok, time)');
+	my $last_test = $dbh->selectrow_array('SELECT id FROM results WHERE time = (SELECT MAX(time) from results)');
+	$dbh->do('INSERT INTO results (count,ok,nok,time) VALUES (?,?,?,?)',undef,$all,$ok,$all-$ok,time);
+	my $curr_test = $dbh->last_insert_id(undef,undef,'results','id');
+	$dbh->do("CREATE TABLE test_$curr_test (comic,result,strips_counted)");
+	my $sth = $dbh->prepare("INSERT INTO test_$curr_test (comic,result,strips_counted) VALUES (?,?,?)");
+	$sth->execute_array(undef,[keys %results],[map {$results{$_}->{result}} keys %results],[map {$results{$_}->{count}} keys %results]);
+	if ($last_test) {
+		my ($lall,$lok,$lnok) = $dbh->selectrow_array('SELECT count,ok,nok FROM results WHERE id = ?',undef,$last_test);
+		my %lres = %{$dbh->selectall_hashref("SELECT * FROM test_$last_test",'comic')};
+		my @cmcs = (scalar keys %results)>(scalar keys %lres)?keys %results: keys %lres;
+		foreach my $cmc (@cmcs) {
+			if (!$results{$cmc}->{result}) {
+				say "$cmc was not tested anymore";
+				next;
+			}
+			if (!$lres{$cmc}->{result}) {
+				say "$cmc was newly tested";
+				next;
+			}
+			if ((!defined $results{$cmc}->{count}) or  (!defined $lres{$cmc}->{strips_counted})) {
+				say "$cmc has no count defined?!";
+			}
+			elsif ($results{$cmc}->{count} != $lres{$cmc}->{strips_counted}) {
+				say "count changed from " . $lres{$cmc}->{strips_counted} . " to " . $results{$cmc}->{count};
+			}
+			if ($results{$cmc}->{result} eq 'ok' and $lres{$cmc}->{result} eq 'ok') {
+				next; #both ok
+			}
+			if ($results{$cmc}->{result} eq $lres{$cmc}->{result}) {
+				next; #no change both error
+			}
+			say "$cmc was " . $lres{$cmc}->{result} . " and is now " . $results{$cmc};
+		}		
+		say "lall: $lall lok: $lok lnok: $lnok"; 
+	}
+	$dbh->commit;
+	$dbh->disconnect;
+}
+
+
 
 say "all: $all ok: $ok nok: " . ($all - $ok); 
