69547dccfa539356d64d7d24c4b57c2d4f48f8ea
ragnar <ragnar@rmgk.de>
2010-10-01 13:40:48 +0200

    improved log output

--- lib/Log.pm
+++ lib/Log.pm
@@ -24,49 +24,60 @@ sub new {
 sub log {
 	my ($s) = shift;
 	my @call = caller 1;
-	say "\t". join("\t", @call[2,0]) . ":\t" . join '', @_;
+	my $line = $call[2];
+	my $module = $call[0];
+	$module =~ s/\w+::/:/g;
+	#getting the call depth
+	my $d = 1;
+	while(caller(++$d)){}; 
+	say $line,
+		' ' x (3-length($line)), #number of spaces depends on line numer length
+		'-' x ($d-2) , '>', #add call depth indicator
+		$module, 
+		"\t" x ((6/length($module))+1.5), #uhm well it works for now
+		join '', @_; #the message itself
 } 
 
 #$msg
 sub trace { 
 	my ($s) = shift;
 	return if ($TRACE < $s->{level});
-	print 'TRACE';
+	print 'TRACE ';
 	$s->log(@_);
 }
 #$msg
 sub debug { 
 	my ($s) = shift;
 	return if ($DEBUG < $s->{level});
-	print 'DEBUG';
+	print 'DEBUG ';
 	$s->log(@_);
 }
 #$msg
 sub info { 
 	my ($s) = shift;
 	return if ($INFO < $s->{level});
-	print 'INFO';
+	print 'INFO  ';
 	$s->log(@_);
 }
 #$msg
 sub warn { 
 	my ($s) = shift;
 	return if ($WARN < $s->{level});
-	print 'WARN';
+	print 'WARN  ';
 	$s->log(@_);
 }
 #$msg
 sub error { 
 	my ($s) = shift;
 	return if ($ERROR < $s->{level});
-	print 'ERROR';
+	print 'ERROR ';
 	$s->log(@_);
 }
 #$msg
 sub fatal { 
 	my ($s) = shift;
 	return if ($FATAL < $s->{level});
-	print 'FATAL';
+	print 'FATAL ';
 	$s->log(@_);
 }
 
