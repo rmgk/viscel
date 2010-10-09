e6654e3339e1027a54731c7eba300a184b36ecce
ragnar <ragnar@rmgk.de>
2010-10-09 23:18:57 +0200

    display chapter of current entity in page title

--- lib/RequestHandler.pm
+++ lib/RequestHandler.pm
@@ -195,7 +195,7 @@ sub view {
 		return "view redirect";
 	} 
 	
-	my $html = html_header('view',$pos);
+	my $html = html_header('view',$pos .' - '. $ent->chapter);
 	if ($r->method eq 'POST') {
 		$l->debug("set bookmark of $id to $pos");
 		UserPrefs->section('bookmark')->set($id,$pos);
