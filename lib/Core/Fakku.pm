6673cc7bac7d6176e0afb1e4d419aca458b608c6
ragnar <ragnar@rmgk.de>
2010-11-22 19:36:52 +0100

    fixed core::fakku

--- lib/Core/Fakku.pm
+++ lib/Core/Fakku.pm
@@ -22,15 +22,13 @@ sub _create_list {
 		my $a = $main->look_down('_tag'=> 'div', 'class' => 'manga_row1')->look_down('_tag' => 'a');
 		my $href = $a->attr('href');
 		my $name = HTML::Entities::encode($a->as_trimmed_text());
-		my ($id) = ($href =~ m'id=(\d+)'i);
-		unless ($id) {
+		my ($num) = ($href =~ m'id=(\d+)'i);
+		unless ($num) {
 			$l->debug("could not parse $href");
 			next;
 		}
-		$href = "http://www.fakku.net/viewonline.php?id=$id&page=1";
-		$id =~ s/\W/_/g;
-		$id = 'Fakku_' . $id;
-		$clist{$id} = {url_start => $href, name => $name};
+		my $id = 'Fakku_' . $num;
+		$clist{$id} = {url_start => "001:$num" , name => $name}; #url start is start state
 		$clist{$id}->{Series} = $main->look_down('_tag'=> 'div', 'class' => 'manga_row2')->look_down('_tag' => 'div',class => 'item2')->as_trimmed_text(extra_chars => '\xA0');
 		my $trans_link = $main->look_down('_tag'=> 'div', 'class' => 'manga_row2')->look_down('_tag' => 'span',class => 'english')->look_down(_tag => 'a');
 		$clist{$id}->{Scanlator} = $trans_link->as_trimmed_text(extra_chars => '\xA0') if $trans_link;
@@ -60,25 +58,52 @@ package Core::Fakku::Spot;
 
 use parent -norequire, 'Core::Template::Spot';
 
+#makes preparations to find objects
+sub mount {
+	my ($s) = @_;
+	$l->trace('mount ' . $s->{id});
+	my ($pos,$id,$max,$section,$folder) = split(':',$s->{state});
+	unless ($max) {
+		($max,$section,$folder) = $s->_mount_parse($id);
+		unless ($max) {
+			$l->warn('could not parse page');
+			$s->{fail} = 'could not parse page';
+			return undef;
+		}
+	}
+	if ($pos > $max) {
+		$s->{fail} = 'last page';
+		return undef;
+	}
+	$s->{page_url} = 'http://www.fakku.net/viewonline.php?id='.$id . '#page='.$pos;
+	$s->{src} = 'http://c.fakku.net/manga/' . $section . '/' . $folder . '/images/'. $pos . '.jpg';
+	$pos++;
+	$pos = ('0' x (3-length($pos))) . $pos; #leading zeroes
+	$s->{next} = join(':', $pos,$id,$max,$section,$folder);
+	$s->{fail} = undef;
+	return 1;
+}
+
+
 #$tree
 #parses the page to mount it
 sub _mount_parse {
-	my ($s,$tree) = @_;
-	my $img = $tree->look_down(_tag => 'div', id=>'content')->look_down(_tag=>'img',src => qr'fakku.net/manga');
-	unless ($img) {
-		$l->error('could not get image');
-		$s->{fail} = 'could not get image';
+	my ($s,$id) = @_;
+	
+	my $url = 'http://www.fakku.net/viewonline.php?id='.$id;
+	my $page = DlUtil::get($url);
+	if (!$page->is_success() or !$page->header('Content-Length')) {
+		$l->error("error get: ", $url);
 		return undef;
 	}
-	map {$s->{$_} = $img->attr($_)} qw( src title alt width height);
-	$s->{src} = URI->new_abs($s->{src},$s->{state})->as_string;
-	my $a = $img->look_up(_tag=>'a');
-	if ($a) {
-		$s->{next} = $a->attr('href');
-		$s->{next} = URI->new_abs($s->{next} ,$s->{state})->as_string;
+	my $content = $page->decoded_content();
+	
+	#say $content;
+
+	if ($content =~ /"section":"(\w+)".*?"folder":"(\w+)".*?(\d\d\d)\.thumb\.\w{3,4}"\]\}\;/m) {
+		return $3,$1,$2
 	}
-	($s->{filename}) = ($s->{src} =~ m'/([^/]+)$'i) ;
-	return 1;
+	return undef;
 }
 
 1;
