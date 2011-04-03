5940921e11239704ce9ccfe476c260d18d65a938
ragnar <ragnar@rmgk.de>
2011-04-03 14:46:09 +0200

    refactored UserPrefs into ConfigINI

--- lib/Core/Template.pm
+++ lib/Core/Template.pm
@@ -44,7 +44,7 @@ sub save_clist {
 	$pkg = ref($pkg) || $pkg;
 	$l->trace("save clist ", $pkg);
 	no strict 'refs';
-	return UserPrefs::save_file($pkg,\%$pkg);
+	return ConfigINI::save_file(Globals::cachedir,$pkg,\%$pkg);
 }
 
 #initialises the core and loads collection list
@@ -58,7 +58,7 @@ sub init {
 #tries to load the collection list from file, creates it if it cant be found
 sub _load_list {
 	my ($pkg) = @_;
-	$pkg->clist(UserPrefs::parse_file($pkg));
+	$pkg->clist(ConfigINI::parse_file(Globals::cachedir,$pkg));
 	if ($pkg->clist()) {
 		$l->debug('loaded ' . scalar($pkg->clist()) . ' collections');
 		return 1;
--- lib/UserPrefs.pm
+++ lib/UserPrefs.pm
@@ -1,6 +1,6 @@
 #!perl
 #This program is free software. You may redistribute it under the terms of the Artistic License 2.0.
-package UserPrefs v1.1.0;
+package UserPrefs v1.3.0;
 
 use 5.012;
 use warnings;
@@ -8,7 +8,6 @@ use warnings;
 use Log;
 use Globals;
 
-my $l = Log->new();
 my %data;
 my $changed = 0;
 my $dir;
@@ -16,51 +15,15 @@ my $default_file;
 
 #initialises the preferences
 sub init {
-	$l->trace('initialise config');
+	Log->trace('initialise config');
 	$dir = shift // Globals::userdir();
 	$default_file = shift // 'user';
 	unless (-e $dir or mkdir $dir) {
-		$l->error('could not create cache dir ' , $dir);
+		Log->error('could not create cache dir ' , $dir);
 		return;
 	}
-	%data = %{parse_file($default_file)};
+	%data = %{ConfigINI::parse_file($default_file)};
 	return 1;
-}
-
-#$file -> \%cfg;
-sub parse_file {
-	my ($file) = @_;
-	$file =~ s/\W+/_/g;
-	$l->trace("parse config file ",$file);
-	$file = $dir.$file.'.ini';
-	unless (-e $file) {
-		$l->trace("$file not found create new config");
-		return {};
-	}
-	my $block;
-	my %cfg;
-	open (my $fh, '<:encoding(UTF-8)', $file);
-	while (my $line = <$fh>) {
-		chomp($line);
-		if ($line =~ /^\s*\[\s*(?<block>[\w:]+)\s*\]\s*$/) {
-			$block = $+{block};
-			next;
-		}
-		next if $line =~ /^\s*\[;#]/;
-		next if $line =~ /^\s*$/;
-		next if length $line == 0;
-		
-		my ($what,$is) = split(/=/, $line, 2);
-		$what =~ s/^\s*//g;
-		$what =~ s/\s*$//g;
-		#keeping value exactly as is, with whitespaces
-		#$is =~ s/^\s*//g;
-		#$is =~ s/\s*$//g;
-		
-		$cfg{$block}->{$what} = $is;
-	}
-	close ($fh);
-	return \%cfg;
 }
 
 #$class, $sect -> $self
@@ -68,9 +31,9 @@ sub parse_file {
 sub section {
 	my ($class,$sect) = @_;
 	$sect ||= caller;
-	$l->trace("userprefs handle for ", $sect); 
+	Log->trace("userprefs handle for ", $sect); 
 	unless ($data{$sect}) {
-		$l->debug("create $sect");
+		Log->debug("create $sect");
 		$data{$sect} = {};
 	}
 	return bless \$sect, $class;
@@ -96,7 +59,7 @@ sub get {
 #sets the value of key
 sub set {
 	my ($s,$key,$value) = @_;
-	$l->trace("set sect: ", $$s, " key: $key to value: ",$value);
+	Log->trace("set sect: ", $$s, " key: $key to value: ",$value);
 	$data{$$s}->{$key} = $value;
 	$changed = 1;
 	return $value;
@@ -105,41 +68,8 @@ sub set {
 #saves the config
 sub save {
 	return unless $changed;
-	save_file($default_file,\%data);
+	ConfigINI::save_file($dir,$default_file,\%data);
 	$changed = 0;
-}
-
-#$file,$cfg 
-#saves $cfg to $file
-sub save_file {
-	my ($file,$cfg) = @_;
-	$file =~ s/\W+/_/g;
-	$l->trace('save config to ', $file);
-	$file = $dir.$file.'.ini';
-	if (open (my $fh, '>:encoding(UTF-8)',$file)) {
-		print $fh as_string($cfg);
-		close $fh;
-		return 1;
-	}
-	$l->warn('could not open ', $file);
-	return;
-}
-
-#returns the %data as a string
-sub as_string {
-	my ($cfg,$sect) = @_;
-	$sect = $$sect if ref $sect;
-	if ($sect) {
-		return "[$sect]\n\t" . 
-			join "\n\t",
-				map { $_ . '=' . $cfg->{$sect}->{$_} }
-					sort grep {defined $cfg->{$sect}->{$_} and $cfg->{$sect}->{$_} ne ''} keys %{$cfg->{$sect}};
-	}
-	else {
-		return join "\n",
-			map { as_string($cfg,$_) }
-				 sort grep {keys %{$cfg->{$_}}} keys %$cfg;
-	}
 }
 
 1;
