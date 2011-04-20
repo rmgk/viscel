b12b5393c26ceb024cdf9b99495e66975fc68046
ragnar <ragnar@rmgk.de>
2011-04-21 00:03:18 +0200

    save user prefs only if something really is deleted

--- src/UserPrefs.pm
+++ src/UserPrefs.pm
@@ -73,7 +73,7 @@ sub remove {
 	$sect = $$sect if ref $sect;
 	Log->trace("delete sect: $sect key: $key");
 	my $result = delete $data{$sect}->{$key};
-	$changed = 1;
+	$changed = 1 if defined $result;
 	return $result;
 }
 
