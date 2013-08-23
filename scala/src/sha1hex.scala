eed01afd8e095327756137f16cc7b762164df1e6
ragnar <ragnar@rmgk.de>
2013-08-23 23:42:47 +0200

    delete some unused files

--- scala/src/sha1hex.scala
+++
@@ -1,9 +0,0 @@
-package viscel
-
-import java.security.MessageDigest
-
-object sha1hex {
-	val digester = MessageDigest.getInstance("SHA1")
-	def apply(b: Array[Byte]) = digester.digest(b).map { "%02X" format _ }.mkString
-}
-
