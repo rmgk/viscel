21e6bbfd89d36cadce8ec3cb21ac690b3941177a
ragnar <ragnar@rmgk.de>
2013-09-06 16:39:08 +0200

    suppress timeout stacktraces for shutdown

--- scala/src/core/Clockwork.scala
+++ scala/src/core/Clockwork.scala
@@ -2,26 +2,27 @@ package viscel.core
 
 import akka.actor.{ ActorSystem, Props, Actor }
 import akka.io.IO
+import akka.pattern.AskTimeoutException
 import com.typesafe.scalalogging.slf4j.Logging
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.neo4j.graphdb.Direction
+import scala.collection.JavaConversions._
 import scala.concurrent._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util._
+import scalax.io._
 import spray.can.Http
 import spray.client.pipelining._
-import spray.http.Uri
-import viscel._
-import viscel.store._
+import spray.http.ContentType
 import spray.http.HttpHeaders.`Content-Type`
 import spray.http.HttpHeaders.Location
 import spray.http.HttpRequest
 import spray.http.HttpResponse
-import spray.http.ContentType
-import scalax.io._
-import scala.collection.JavaConversions._
-import org.neo4j.graphdb.Direction
+import spray.http.Uri
+import viscel._
+import viscel.store._
 
 class Clockwork(val iopipe: SendReceive) extends Logging {
 
@@ -69,10 +70,11 @@ class Clockwork(val iopipe: SendReceive) extends Logging {
 				case e: NormalStatus => logger.info(s"${ocore.id} complete ${e}")
 				case e: FailedStatus =>
 					logger.info(s"${ocore.id} failed ${e}"); e.printStackTrace
+				case e: AskTimeoutException => logger.info(s"${ocore.id} timed out (system shutdown?)")
 				case e: Throwable => logger.info(s"${ocore.id} unexpected error ${e}"); e.printStackTrace
 			}
 		}
 	}
 
-	def test() = for (core <- Seq(PhoenixRequiem /*, MarryMe, InverlochArchive, TwokindsArchive, Avengelyne*/ )) fullArchive(core)
+	def test() = for (core <- Seq(PhoenixRequiem, MarryMe, InverlochArchive, TwokindsArchive, Avengelyne)) fullArchive(core)
 }
