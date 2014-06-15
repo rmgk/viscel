c86df52d1183e67f8a686ece445e93d7be508120
ragnar <ragnar@rmgk.de>
2014-06-15 13:57:52 +0200

    sort relations and labels

--- scala/src/main/scala/viscel/store/RelLabel.scala
+++ scala/src/main/scala/viscel/store/RelLabel.scala
@@ -6,16 +6,16 @@ object label {
 
 	class SimpleLabel(val name: String) extends Label
 
-	case object Unlabeled extends SimpleLabel("Unlabeled")
-	case object Collection extends SimpleLabel("Collection")
-	case object Element extends SimpleLabel("Element")
-	case object User extends SimpleLabel("User")
+	case object Blob extends SimpleLabel("Blob")
 	case object Bookmark extends SimpleLabel("Bookmark")
-	case object Config extends SimpleLabel("Config")
 	case object Chapter extends SimpleLabel("Chapter")
+	case object Collection extends SimpleLabel("Collection")
+	case object Config extends SimpleLabel("Config")
+	case object Element extends SimpleLabel("Element")
 	case object Page extends SimpleLabel("Archive")
 	case object Structure extends SimpleLabel("Structure")
-	case object Blob extends SimpleLabel("Blob")
+	case object Unlabeled extends SimpleLabel("Unlabeled")
+	case object User extends SimpleLabel("User")
 
 }
 
@@ -23,18 +23,18 @@ object rel {
 
 	class SimpleRelationshipType(val name: String) extends RelationshipType
 
-	case object next extends SimpleRelationshipType("next")
-	case object prev extends SimpleRelationshipType("prev")
-	case object payload extends SimpleRelationshipType("payload")
-	case object parent extends SimpleRelationshipType("parent")
-	case object child extends SimpleRelationshipType("child")
+	case object archive extends SimpleRelationshipType("archive")
+	case object blob extends SimpleRelationshipType("blob")
+	case object bookmark extends SimpleRelationshipType("bookmark")
 	case object bookmarked extends SimpleRelationshipType("bookmarked")
 	case object bookmarks extends SimpleRelationshipType("bookmarks")
-	case object bookmark extends SimpleRelationshipType("bookmark")
+	case object child extends SimpleRelationshipType("child")
+	case object describes extends SimpleRelationshipType("describes")
 	case object first extends SimpleRelationshipType("first")
 	case object last extends SimpleRelationshipType("last")
-	case object archive extends SimpleRelationshipType("archive")
-	case object describes extends SimpleRelationshipType("describes")
-	case object blob extends SimpleRelationshipType("blob")
+	case object next extends SimpleRelationshipType("next")
+	case object parent extends SimpleRelationshipType("parent")
+	case object payload extends SimpleRelationshipType("payload")
+	case object prev extends SimpleRelationshipType("prev")
 
 }
