aec29d4f00239599965c075318e0df451b590bd3
ragnar <ragnar@rmgk.de>
2014-06-15 03:06:24 +0200

    add twokinds improve wrapper errors

--- scala/src/main/scala/viscel/core/Wrapper.scala
+++ scala/src/main/scala/viscel/core/Wrapper.scala
@@ -1,14 +1,13 @@
 package viscel.core
 
 import com.typesafe.scalalogging.slf4j.StrictLogging
-import org.jsoup.nodes.{ Document, Element }
+import org.jsoup.nodes.{Document, Element}
+import org.scalactic.Accumulation._
 import org.scalactic.TypeCheckedTripleEquals._
 import org.scalactic._
-import org.scalactic.Accumulation._
 
 import scala.collection.JavaConversions._
 import scala.language.implicitConversions
-import scala.util._
 
 trait WrapperTools extends StrictLogging {
 
@@ -17,7 +16,7 @@ trait WrapperTools extends StrictLogging {
 		if (res.isEmpty) None else Some(k -> res)
 	}
 
-	def imgToElement(img: Element): ElementDescription = ElementDescription(
+	def imgToElement(img: Element): ElementContent = ElementContent(
 		source = img.attr("abs:src"),
 		origin = img.baseUri,
 		props = (getAttr(img, "alt") ++
@@ -25,14 +24,19 @@ trait WrapperTools extends StrictLogging {
 			getAttr(img, "width") ++
 			getAttr(img, "height")).toMap)
 
+	def caller(n: Int) = {
+		val c = Thread.currentThread().getStackTrace()(n)
+		s"${ c.getClassName }#${ c.getMethodName }:${ c.getLineNumber }"
+	}
+
 	def selectUnique(from: Element, query: String): Element Or One[ErrorMessage] = from.select(query) match {
-		case rs if rs.size > 2 => Bad(One(s"query not unique ($query) on (${from.baseUri}, ${from.tag}, #${from.id}, .${from.classNames})"))
-		case rs if rs.size < 1 => Bad(One(s"query not found ($query) on (${from.baseUri}, ${from.tag}, #${from.id}, .${from.classNames})"))
+		case rs if rs.size > 2 => Bad(One(s"query not unique ($query) at (${ caller(5) }) on (${ from.baseUri }, ${ from.tag }, #${ from.id }, .${ from.classNames })"))
+		case rs if rs.size < 1 => Bad(One(s"query not found ($query) at (${ caller(5) }) on (${ from.baseUri }, ${ from.tag }, #${ from.id }, .${ from.classNames })"))
 		case rs => Good(rs(0))
 	}
 
 	def selectSome(from: Element, query: String): Seq[Element] Or One[ErrorMessage] = from.select(query) match {
-		case rs if rs.size < 1 => Bad(One(s"query did not match ($query) on (${from.baseUri}, ${from.tag}, #${from.id}, .${from.classNames})"))
+		case rs if rs.size < 1 => Bad(One(s"query did not match ($query) at (${ caller(5) }) on (${ from.baseUri }, ${ from.tag }, #${ from.id }, .${ from.classNames })"))
 		case rs => Good(rs.toIndexedSeq)
 	}
 
@@ -43,7 +47,7 @@ trait WrapperTools extends StrictLogging {
 
 	def chapter(name: String, children: Seq[Description] = Seq(), next: Description = EmptyDescription, props: Map[String, String] = Map()): StructureDescription =
 		StructureDescription(
-			payload = ChapterDescription(name, props),
+			payload = ChapterContent(name, props),
 			children = children,
 			next = next)
 }
@@ -56,7 +60,7 @@ object Misfile extends Core with WrapperTools with StrictLogging {
 	def name: String = "Misfile"
 
 	def chapterFrom(name: String, first: AbsUri) = StructureDescription(
-		payload = ChapterDescription(name),
+		payload = ChapterContent(name),
 		children = PointerDescription(first, "page") :: Nil)
 
 	def wrapArchive(doc: Document, pd: PointerDescription): StructureDescription Or Every[ErrorMessage] = {
@@ -67,28 +71,62 @@ object Misfile extends Core with WrapperTools with StrictLogging {
 		}
 		withGood(wrapPage(doc, pd), chapters) { (page, chapters) =>
 			StructureDescription(children =
-				page.copy(payload = ChapterDescription("Book #1"))
+				page.copy(payload = ChapterContent("Book #1"))
 					+: chapters)
 		}
 	}
 
 	def wrapPage(doc: Document, pd: PointerDescription): StructureDescription Or Every[ErrorMessage] = {
 		selectUnique(doc, ".comiclist table.wide_gallery").flatMap { clist =>
-			val elements = selectSome(clist, "[id~=^comic_\\d+$] .picture a").map {
-				_.map { anchor =>
-					StructureDescription(ElementDescription(origin = anchor.attr("abs:href"), source = anchor.select("img").attr("abs:src").replace("/t", "/")))
+			val elements_? = selectSome(clist, "[id~=^comic_\\d+$] .picture a").flatMap { anchors =>
+				anchors.validatedBy { anchor =>
+					selectUnique(anchor, "img").map { img =>
+						StructureDescription(ElementContent(origin = anchor.attr("abs:href"), source = img.attr("abs:src").replace("/t", "/")))
+					}
 				}
 			}
 			val next = doc.select("a.next") match {
 				case ns if ns.size > 0 => PointerDescription(ns(0).attr("abs:href"), "page")
 				case ns => EmptyDescription
 			}
-			elements.map { elem => StructureDescription(children = elem, next = next) }
+			elements_?.map { elements => StructureDescription(children = elements, next = next) }
 		}
 	}
 
 	def wrap(doc: Document, pd: PointerDescription): Description = Description.fromOr(pd.pagetype match {
 		case "archive" => wrapArchive(doc, pd)
 		case "page" => wrapPage(doc, pd)
+	})
+}
+
+object Twokinds extends Core with WrapperTools with StrictLogging {
+	def archive = StructureDescription(children =
+		PointerDescription("http://twokinds.keenspot.com/?p=archive", "archive") ::
+			PointerDescription("http://twokinds.keenspot.com/index.php", "main") :: Nil)
+
+	def id: String = "NX_Twokinds"
+
+	def name: String = "Twokinds"
+
+	def chapterFrom(name: String, first: AbsUri) = StructureDescription(
+		payload = ChapterContent(name),
+		children = PointerDescription(first, "page") :: Nil)
+
+	def wrapArchive(doc: Document, pd: PointerDescription): StructureDescription Or Every[ErrorMessage] = {
+		selectSome(doc, ".archive .chapter").flatMap { chapters =>
+			chapters.validatedBy { chapter =>
+				val title_? = selectUnique(chapter, "h4").map(_.ownText())
+				val links_? = selectSome(chapter, "a").map(_.map(_.attr("abs:href")))
+				withGood(title_?, links_?) { (title, links) =>
+					StructureDescription(payload = ChapterContent(title), children = links.map(PointerDescription(_, "page")))
+				}
+			}
+		}.map { chapters => StructureDescription(children = chapters) }
+	}
+
+	def wrap(doc: Document, pd: PointerDescription): Description = Description.fromOr(pd.pagetype match {
+		case "archive" => wrapArchive(doc, pd)
+		case "page" => selectUnique(doc, "#cg_img img").map { img =>	StructureDescription(payload = imgToElement(img))	}
+		case "main" => selectUnique(doc, ".comic img").map { img => StructureDescription(payload = imgToElement(img)) }
 	})
 }
