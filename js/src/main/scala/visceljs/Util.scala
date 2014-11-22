2a034fd201e2ce6816cab90ca543e6938fa913cb
ragnar <ragnar@rmgk.de>
2014-11-23 00:07:45 +0100

    auto format

--- js/src/main/scala/visceljs/Util.scala
+++ js/src/main/scala/visceljs/Util.scala
@@ -16,13 +16,13 @@ object Util {
 
 	def path_main = "/"
 	def path_css = "/css"
-	def path_asset(nr: Narration, absPos: Int) = s"#${nr.id}/$absPos"
+	def path_asset(nr: Narration, absPos: Int) = s"#${ nr.id }/$absPos"
 	def path_search = "/s"
 	def path_blob(blob: String) = s"/blob/${ blob }"
-	def path_front(nar: Narration) = s"#${nar.id}"
-//	def path_raw(vn: Coin) = s"/r/${ vn.nid }"
+	def path_front(nar: Narration) = s"#${ nar.id }"
+	//	def path_raw(vn: Coin) = s"/r/${ vn.nid }"
 	def path_stop = "/stop"
-//	def path_core(core: Narrator) = s"/f/${ core.id }"
+	//	def path_core(core: Narrator) = s"/f/${ core.id }"
 	def path_scripts = "/viscel.js"
 
 	val class_main = cls := "main"
@@ -38,21 +38,21 @@ object Util {
 
 	def link_main(ts: Frag*) = a(href := path_main)(ts)
 	def link_stop(ts: Frag*) = a(href := path_stop)(ts)
-//	//def link_front(collection: CollectionNode, ts: Frag*) = a(href := path_front(collection))(ts)
-	def link_asset(nar: Narration, gallery: Gallery[Asset], ts: Frag*) = a(href := path_asset(nar, gallery.pos + 1))(ts)(onclick := {() =>
+	//	//def link_front(collection: CollectionNode, ts: Frag*) = a(href := path_front(collection))(ts)
+	def link_asset(nar: Narration, gallery: Gallery[Asset], ts: Frag*) = a(href := path_asset(nar, gallery.pos + 1))(ts)(onclick := { () =>
 		Viscel.setBody("view", ViewPage.gen(gallery, nar))
 	})
 
-	def go_front(nar: Narration) = {() =>
+	def go_front(nar: Narration) = { () =>
 		for (bm <- Viscel.bookmarks; fullNarration <- Viscel.completeNarration(nar))
 			Viscel.setBody("front", FrontPage.genIndex(bm(nar.id), fullNarration))
 	}
 
 	def link_front(nar: Narration, ts: Frag*): Frag = a(href := path_front(nar))(ts)(onclick := go_front(nar))
-//	def link_node(vn: Option[Coin], ts: Frag*): Frag = vn.map { link_node(_, ts: _*) }.getOrElse(span(ts: _*))
-//	def link_raw(vn: Coin, ts: Frag*): Frag = a(href := path_raw(vn))(ts)
-//	// def link_node(en: Option[ElementNode], ts: Frag*): Frag = en.map{n => link_view(n.collection.id, n.position, ts)}.getOrElse(ts)
-//	def link_core(core: Narrator): Frag = a(href := path_core(core))(core.name)
+	//	def link_node(vn: Option[Coin], ts: Frag*): Frag = vn.map { link_node(_, ts: _*) }.getOrElse(span(ts: _*))
+	//	def link_raw(vn: Coin, ts: Frag*): Frag = a(href := path_raw(vn))(ts)
+	//	// def link_node(en: Option[ElementNode], ts: Frag*): Frag = en.map{n => link_view(n.collection.id, n.position, ts)}.getOrElse(ts)
+	//	def link_core(core: Narrator): Frag = a(href := path_core(core))(core.name)
 
 	def set_bookmark(nar: Narration, pos: Int, ts: Frag*): HtmlTag = p(ts)(onclick := { () =>
 		Viscel.setBookmark(nar, pos)
@@ -70,7 +70,7 @@ object Util {
 
 	def form_search(narrations: List[Narration], results: HTMLElement): HtmlTag = {
 		var filtered = Seq[Narration]()
-		lazy val inputField: HTMLInputElement = input(`type` := "textfield", onkeyup := {() =>
+		lazy val inputField: HTMLInputElement = input(`type` := "textfield", onkeyup := { () =>
 			results.innerHTML = ""
 			val query = inputField.value.toString.toLowerCase
 			if (!query.isEmpty) {
@@ -80,7 +80,7 @@ object Util {
 				()
 			}
 		}).render
-		form(inputField, action := "")(onsubmit := {() => filtered.headOption.foreach(go_front(_)()); false})(id := "searchform")
+		form(inputField, action := "")(onsubmit := { () => filtered.headOption.foreach(go_front(_)()); false })(id := "searchform")
 	}
 
 	def blobToImg(asset: Asset): HtmlTag = {
@@ -96,7 +96,7 @@ object Util {
 
 	def keys(obj: js.Object): List[String] = {
 		var res = List[String]()
-		js.Object.keys(obj).map[Unit]{(s: String) => res ::= s}
+		js.Object.keys(obj).map[Unit] { (s: String) => res ::= s }
 		res.reverse
 	}
 
