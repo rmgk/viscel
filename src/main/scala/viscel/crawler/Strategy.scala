1f430f45b854fba6c23840f428b74eccab154ed2
ragnar <ragnar@rmgk.de>
2014-11-23 02:35:52 +0100

    allow combination of strategies via andthen

--- src/main/scala/viscel/crawler/Strategy.scala
+++ src/main/scala/viscel/crawler/Strategy.scala
@@ -5,4 +5,10 @@ import viscel.database.Ntx
 
 trait Strategy {
 	def run(implicit ntx: Ntx): Option[(Node, Strategy)]
+	def andThen(other: Strategy): Strategy = new Strategy {
+		override def run(implicit ntx: Ntx): Option[(Node, Strategy)] = this.run match {
+			case Some((node, next)) => Some(node -> next.andThen(other))
+			case None => other.run
+		}
+	}
 }
