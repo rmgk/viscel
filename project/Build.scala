3f9919cde397b0d16f3fab2a7455a9674cf4941f
ragnar <ragnar@rmgk.de>
2014-12-06 00:51:07 +0100

    update version to 5.4

--- project/Build.scala
+++ project/Build.scala
@@ -26,7 +26,7 @@ object Build extends sbt.Build {
 object Settings {
 	lazy val common = List(
 
-		version := "5.3.0",
+		version := "5.4.0",
 		scalaVersion := "2.11.4",
 		SourceGeneration.caseCodecs,
 
