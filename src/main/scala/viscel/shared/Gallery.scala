594c05905a6a04904dfdf669b22d577c50b123c3
ragnar <ragnar@rmgk.de>
2015-01-17 00:22:04 +0100

    gallery is now backed by an array instead of a list

--- src/main/scala/viscel/shared/Gallery.scala
+++ src/main/scala/viscel/shared/Gallery.scala
@@ -2,24 +2,29 @@ package viscel.shared
 
 import upickle.{Reader, Writer, writeJs}
 
-case class Gallery[+A](left: List[A], right: List[A]) {
-	lazy val toList: List[A] = left reverse_::: right
-	lazy val first: Gallery[A] = Gallery(Nil, toList)
-	lazy val end: Gallery[A] = Gallery(right reverse_::: left, Nil)
-	def get: Option[A] = right.headOption
-	def next(n: Int): Gallery[A] = Gallery(right.take(n) reverse_::: left, right.drop(n))
-	def prev(n: Int): Gallery[A] = Gallery(left.drop(n), left.take(n) reverse_::: right)
-	lazy val size: Int = pos + right.size
-	lazy val pos: Int = left.size
-	override lazy val toString: String = s"Gallery(${ left.reverse.mkString(", ") } | ${ right.mkString(", ") })"
-	def isFirst: Boolean = left.isEmpty
-	def isEnd: Boolean = right.isEmpty
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+
+final class Gallery[+A] private (val pos: Int, entries: Array[A]) {
+	def copy(position: Int) = new Gallery(position, entries)
+	def toList: List[A] = List(entries: _*)
+	def first: Gallery[A] = copy(0)
+	def end: Gallery[A] = copy(entries.length)
+	def get: Option[A] = if (pos < size) Some(entries(pos)) else None
+	def next(n: Int): Gallery[A] = copy(if (pos + n < size) pos + n else size)
+	def prev(n: Int): Gallery[A] = copy(if (pos - n  >= 0) pos - n else 0)
+	def size: Int = entries.length
+	override def toString: String = s"Gallery(${ Predef.genericWrapArray(entries).mkString(", ") })"
+	def isFirst: Boolean = pos == 0
+	def isEnd: Boolean = pos == size
 	def isEmpty: Boolean = isFirst && isEnd
 }
 
 object Gallery {
-	def fromList[A](l: List[A]): Gallery[A] = Gallery(Nil, l)
+	def fromList[A: ClassTag](l: List[A]): Gallery[A] = fromArray(l.toArray)
+	def fromArray[A](a: Array[A]): Gallery[A] = new Gallery(0, a)
 	val empty: Gallery[Nothing] = Gallery.fromList(Nil)
-	implicit def galleryR[A: Reader]: Reader[Gallery[A]] = Reader[Gallery[A]](upickle.SeqishR[A, List].read.andThen(fromList))
+	implicit def galleryR[A: Reader : ClassTag]: Reader[Gallery[A]] = Reader[Gallery[A]](upickle.SeqishR[A, mutable.WrappedArray].read.andThen(w => fromArray(w.toArray)))
 	implicit def galleryW[A: Writer]: Writer[Gallery[A]] = Writer[Gallery[A]](g => writeJs(g.toList))
 }
