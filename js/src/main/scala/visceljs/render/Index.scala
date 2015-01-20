e60f70a1d461febd585827567df1cbbc05d0a5c3
ragnar <ragnar@rmgk.de>
2015-01-20 21:28:30 +0100

    add counts to index

--- js/src/main/scala/visceljs/render/Index.scala
+++ js/src/main/scala/visceljs/render/Index.scala
@@ -13,27 +13,29 @@ import scalatags.JsDom.tags.SeqFrag
 object Index {
 	def gen(bookmarks: Map[String, Int], narrations: Map[String, Narration]): Body = {
 
-		val result: List[(Narration, Int, Int)] =
-			bookmarks.map { case (id, pos) =>
+		val bookmarkedNarrations: List[(Narration, Int, Int)] =
+			bookmarks.toList.map { case (id, pos) =>
 				narrations.get(id).map { nr =>
 					(nr, pos, nr.size - pos)
 				}
-			}.toList.flatten
+			}.flatten
 
-		val (hasNewPages, isCurrent) = result.partition(_._3 > 0)
+		val (hasNewPages, isCurrent) = bookmarkedNarrations.partition(_._3 > 0)
 
 		val unreadTags = hasNewPages.sortBy(-_._3).map {
 			case (nr, pos, unread) => link_front(nr, s"${ nr.name } ($unread)")
 		}
 		val currentTags = isCurrent.sortBy(_._1.name).map {
-			case (nr, pos, unread) => link_front(nr, s"${ nr.name }")
+			case (nr, pos, unread) => link_front(nr, s"${ nr.name }${if (unread >= 0) "" else s" ($unread)"}")
 		}
 
+		val (totalBookmarks, unreadBookmarks) = bookmarkedNarrations.foldLeft((0, 0)){case ((pos, unread), (nar, p, u)) => (pos + p, unread + (if (u > 0) u else 0))}
+		val totalBookmarkedPages = narrations.filterKeys(bookmarks.contains).values.map(_.size).sum
 
 		Body(id = "index", title = "Viscel",
 			frag = List(
-				Make.group("New Pages", unreadTags),
-				Make.group("Bookmarks", currentTags),
+				Make.group(s"New Pages ($unreadBookmarks)", unreadTags),
+				Make.group(s"Bookmarks ($totalBookmarks/$totalBookmarkedPages)", currentTags),
 				Make.navigation(Make.fullscreenToggle("TFS"),link_stop("stop")),
 				Make.searchArea(narrations.values.toList)))
 	}
