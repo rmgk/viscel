5b7580275f2703202aa8d9942faee33f2e6042ed
ragnar <ragnar@rmgk.de>
2015-03-15 01:19:48 +0100

    improve metarartor

--- src/main/scala/viscel/narration/narrators/Fakku.scala
+++ src/main/scala/viscel/narration/narrators/Fakku.scala
@@ -5,6 +5,7 @@ import java.net.URL
 import org.jsoup.nodes.Document
 import org.scalactic.Accumulation._
 import org.scalactic._
+import viscel.Log
 import viscel.narration.Queries._
 import viscel.narration.{Data, Metarrator}
 import viscel.scribe.narration.SelectMore._
@@ -41,11 +42,12 @@ object Fakku {
 			append(chapters_?, next_?)
 		}
 
-		def wrapChapter(doc: Document): List[Story] Or Every[Report] = Selection(doc).many("head script").wrap { scripts =>
-			val jsSrc = scripts.map(_.html()).mkString("\n")
-			val start = jsSrc.indexOf(findStr) + findStr.length
-			val end = jsSrc.indexOf("\n", start) - 1
-			extract { upickle.read[List[String]](jsSrc.substring(start, end)).map(_.replaceAll("thumbs/(\\d+).thumb", "images/$1")).map(new URL(baseURL, _).toString) }.map(_.map { url =>
+		def wrapChapter(doc: Document): List[Story] Or Every[Report] = {
+			val htmlSource = doc.html()
+			val start = htmlSource.indexOf(findStr) + findStr.length
+			val end = htmlSource.indexOf("\n", start) - 1
+			Log.info(s"extract $start, $end,")
+			extract { upickle.read[List[String]](htmlSource.substring(start, end)).map(_.replaceAll("thumbs/(\\d+).thumb", "images/$1")).map(new URL(baseURL, _).toString) }.map(_.map { url =>
 				val extractPos(pos) = url
 				Data.Article(url, s"${ doc.baseUri() }#page=$pos")
 			})
