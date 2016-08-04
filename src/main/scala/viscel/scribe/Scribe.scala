76fa3028577ad91a0fdefc463e16b7e991cc3b94
ragnar <ragnar@rmgk.de>
2016-08-04 15:39:15 +0200

    cache existing descriptions and sizes

--- src/main/scala/viscel/scribe/Scribe.scala
+++ src/main/scala/viscel/scribe/Scribe.scala
@@ -3,7 +3,7 @@ package viscel.scribe
 import java.nio.file.{Files, Path}
 
 import viscel.narration.Narrator
-import viscel.shared.Log
+import viscel.shared.Description
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
@@ -28,6 +28,9 @@ class Scribe(val basepath: Path, val blobs: BlobStore) {
 
 	var bookCache = mutable.HashMap[String, Book]()
 
+	var descriptionCache: Map[String, Description] =
+		Json.load[Map[String, Description]](basepath.resolve("../descriptions.json")).getOrElse(Map())
+
 	def findOrCreate(narrator: Narrator): Book = find(narrator.id).getOrElse{create(narrator)}
 
 	def create(narrator: Narrator): Book = {
@@ -49,8 +52,17 @@ class Scribe(val basepath: Path, val blobs: BlobStore) {
 		}
 	}
 
-	def all(): List[Book] = Files.list(basepath).iterator().asScala.filter(Files.isRegularFile(_)).map{path =>
-		find(path.getFileName.toString).getOrElse(new Book(path))
-	}.toList
+	def allDescriptions(): List[Description] = {
+		Files.list(basepath).iterator().asScala.filter(Files.isRegularFile(_)).map{ path =>
+			val id = path.getFileName.toString
+			descriptionCache.getOrElse(id, {
+				val book = find(id).get
+				val desc = Description(id, book.name, book.size)
+				descriptionCache = descriptionCache.updated(id, desc)
+				Json.store[Map[String, Description]](basepath.resolve("../descriptions.json"), descriptionCache)
+				desc
+			})
+		}.toList
+	}
 
 }
