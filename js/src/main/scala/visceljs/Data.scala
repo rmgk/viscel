603dbc3818ee1779ec89351c48c3fa7ad781a9df
ragnar <ragnar@rmgk.de>
2016-12-06 21:12:23 +0100

    reset fitType default to 0, as it otherwise inferes with media queries

--- js/src/main/scala/visceljs/Data.scala
+++ js/src/main/scala/visceljs/Data.scala
@@ -2,7 +2,7 @@ package visceljs
 
 import viscel.shared.{ImageRef, Contents, Description, Gallery}
 
-case class Data(description: Description, content: Contents, bookmark: Int, fitType: Int = 2) {
+case class Data(description: Description, content: Contents, bookmark: Int, fitType: Int = 0) {
 	def id: String = description.id
 	def pos: Int = content.gallery.pos
 	def gallery: Gallery[ImageRef] = content.gallery
