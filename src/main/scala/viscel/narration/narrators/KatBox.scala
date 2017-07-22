0c70fd90aafd227f4e655d6d414693667fd4b1b2
ragnar <ragnar@rmgk.de>
2017-07-22 19:25:13 +0200

    disable most of katbox … they broke their page

--- src/main/scala/viscel/narration/narrators/KatBox.scala
+++ src/main/scala/viscel/narration/narrators/KatBox.scala
@@ -1,8 +1,7 @@
 package viscel.narration.narrators
 
-import viscel.narration.Queries.{extractMore, queryImages}
+import viscel.narration.Queries.queryImageInAnchor
 import viscel.narration.{Narrator, Templates}
-import viscel.selection.Selection
 
 import scala.collection.immutable.Set
 
@@ -10,35 +9,43 @@ import scala.collection.immutable.Set
 object KatBox {
 
 	val cores: Set[Narrator] = Set(
-		("addictivescience", "Addictive Science"),
-		("ai", "Artificial Incident"),
-		("anthronauts", "Anthronauts!"),
-		("cblue", "Caribbean Blue!"),
-		("desertfox", "Desert Fox"),
-		("dmfa", "DMFA!"),
-		("draconia", "Draconia Chronicles!"),
-		("falsestart", "False Start"),
-		("iba", "Itsy Bitsy Adventures"),
-		("imew", "iMew!"),
-		("knuckleup", "KnuckleUp!"),
-		("laslindas", "Las Lindas!"),
-		("mousechievous", "Mousechievous"),
-		("ourworld", "Our World!"),
-		("paprika", "Paprika"),
-		("peterandcompany", "Peter & Company"),
-		("peterandwhitney", "Peter & Whitney"),
-		("pmp", "Practice makes Perfect"),
-		("projectzero", "Project Zero!"),
-		("rascals", "Rascals!"),
-		("swashbuckled", "Swashbuckled!"),
-		("theeye", "The Eye of Ramalach!"),
-		("tinaofthesouth", "Tina of the South!"),
-		("uberquest", "UberQuest!"),
-		("yosh", "Yosh!")
-	).map { case (id, name) =>
-		Templates.ArchivePage(s"KatBox_$id", s"[KB] $name", s"http://$id.katbox.net/archive/",
-			Selection(_).many("[rel=bookmark]").wrapEach(extractMore).map {_.reverse},
-			queryImages(".webcomic-image img")
-		)
+		("laslindas", "Las Lindas!", "http://laslindas.katbox.net/comic/day-one/")
+	).map { case (id, name, url) =>
+			Templates.SimpleForward(s"KatBox_$id", s"[KB] $name", url,
+				queryImageInAnchor("img[data-webcomic-parent]")
+			)
 	}
+
+//	val cores: Set[Narrator] = Set(
+//		("addictivescience", "Addictive Science"),
+//		("ai", "Artificial Incident"),
+//		("anthronauts", "Anthronauts!"),
+//		("cblue", "Caribbean Blue!"),
+//		("desertfox", "Desert Fox"),
+//		("dmfa", "DMFA!"),
+//		("draconia", "Draconia Chronicles!"),
+//		("falsestart", "False Start"),
+//		("iba", "Itsy Bitsy Adventures"),
+//		("imew", "iMew!"),
+//		("knuckleup", "KnuckleUp!"),
+//		("laslindas", "Las Lindas!"),
+//		("mousechievous", "Mousechievous"),
+//		("ourworld", "Our World!"),
+//		("paprika", "Paprika"),
+//		("peterandcompany", "Peter & Company"),
+//		("peterandwhitney", "Peter & Whitney"),
+//		("pmp", "Practice makes Perfect"),
+//		("projectzero", "Project Zero!"),
+//		("rascals", "Rascals!"),
+//		("swashbuckled", "Swashbuckled!"),
+//		("theeye", "The Eye of Ramalach!"),
+//		("tinaofthesouth", "Tina of the South!"),
+//		("uberquest", "UberQuest!"),
+//		("yosh", "Yosh!")
+//	).map { case (id, name) =>
+//		Templates.ArchivePage(s"KatBox_$id", s"[KB] $name", s"http://$id.katbox.net/archive/",
+//			Selection(_).many("[rel=bookmark]").wrapEach(extractMore).map {_.reverse},
+//			queryImages(".webcomic-image img")
+//		)
+//	}
 }
