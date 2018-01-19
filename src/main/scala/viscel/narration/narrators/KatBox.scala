d76fdf2543ff35273335c98923d6bc92150118c2
ragnar <ragnar@rmgk.de>
2018-01-20 00:44:09 +0100

    fix katbox

--- src/main/scala/viscel/narration/narrators/KatBox.scala
+++ src/main/scala/viscel/narration/narrators/KatBox.scala
@@ -1,7 +1,10 @@
 package viscel.narration.narrators
 
-import viscel.narration.Queries.queryImageInAnchor
-import viscel.narration.{Narrator, Templates}
+import org.jsoup.nodes.{Document, Element}
+import org.scalactic.{Accumulation, Good}
+import viscel.narration.{Contents, Narrator, Queries}
+import viscel.scribe.{ArticleRef, Link, Volatile, Vurl, WebContent}
+import viscel.selection.{ReportTools, Selection}
 
 import scala.collection.immutable.Set
 
@@ -9,11 +12,29 @@ import scala.collection.immutable.Set
 object KatBox {
 
 	val cores: Set[Narrator] = Set(
-		("laslindas", "Las Lindas!", "http://laslindas.katbox.net/comic/day-one/")
-	).map { case (id, name, url) =>
-			Templates.SimpleForward(s"KatBox_$id", s"[KB] $name", url,
-				queryImageInAnchor("img[data-webcomic-parent]")
-			)
+		("laslindas", "Las Lindas!", Some[Vurl]("http://laslindas.katbox.net/las-lindas/"))
+	).map { case (_id, _name, _url) =>
+		new Narrator {
+			override def id: String = s"KatBox_${_id}"
+			override def name: String = _name
+			override def archive: List[WebContent] = List(Link(_url.getOrElse(s"http://${_id}.katbox.net/archive/"), Volatile))
+			override def wrap(doc: Document, link: Link): Contents = {
+				Selection(doc).many("span.archive-link a.webcomic-link").wrapFlat { anchor: Element =>
+					// laslindas at least seems to miss some pages, just skip them
+					if (anchor.childNodeSize() == 0) Good(List.empty)
+					else {
+						val vurl_? = Queries.extractURL(anchor)
+						val img_? = Selection(anchor).unique("img").wrapOne(i => ReportTools.extract(i.absUrl("src")))
+						Accumulation.withGood(img_?, vurl_?) { (img, vurl) =>
+							List(ArticleRef(
+								ref = img.replaceFirst("-\\d+x\\d+\\.", "."),
+								origin = vurl,
+								data = Map()))
+						}
+					}
+				}.map(_.reverse)
+			}
+		}
 	}
 
 //	val cores: Set[Narrator] = Set(
