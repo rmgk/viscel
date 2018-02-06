145f5f8f248e50d07af1e54670217884e11462b6
ragnar <ragnar@rmgk.de>
2018-02-06 22:17:31 +0100

    cleanup

--- src/main/scala/viscel/narration/Templates.scala
+++ src/main/scala/viscel/narration/Templates.scala
@@ -1,9 +1,8 @@
 package viscel.narration
 
 import org.jsoup.nodes.Document
-import viscel.narration.interpretation.NarrationInterpretation
 import viscel.narration.interpretation.NarrationInterpretation.{DocumentWrapper, NarratorADT, PolicyDecision}
-import viscel.scribe.{Link, Volatile, Vurl, WebContent}
+import viscel.scribe.{Link, Volatile, Vurl}
 
 object Templates {
   def ArchivePage(pid: String,
@@ -17,28 +16,10 @@ object Templates {
         volatile = DocumentWrapper(wrapArchive),
         normal = DocumentWrapper(wrapPage)))
 
-  abstract class ArchivePage(start: Vurl,
-                             wrapArchive: Document => Contents,
-                             wrapPage: Document => Contents
-                            ) extends Narrator {
-    override def archive: List[WebContent] = Link(start, Volatile) :: Nil
-    override def wrap(doc: Document, more: Link): Contents = ???
-    override def wrapper: NarrationInterpretation.Wrapper = PolicyDecision(
-        volatile = DocumentWrapper(wrapArchive),
-        normal = DocumentWrapper(wrapPage))
-  }
-
-    def SimpleForward(pid: String,
-                      pname: String,
-                      start: Vurl,
-                      wrapPage: Document => Contents
-                     ): NarratorADT = NarratorADT(pid, pname, Link(start) :: Nil, DocumentWrapper(wrapPage))
-
-    abstract class SimpleForward(start: Vurl,
-                                 wrapPage: Document => Contents
-                                ) extends Narrator {
-      override def archive: List[WebContent] = Link(start) :: Nil
-      override def wrap(doc: Document, more: Link): Contents = ???
-      override def wrapper: NarrationInterpretation.Wrapper = DocumentWrapper(wrapPage)
-    }
-  }
+  def SimpleForward(pid: String,
+                    pname: String,
+                    start: Vurl,
+                    wrapPage: Document => Contents
+                   ): NarratorADT =
+    NarratorADT(pid, pname, Link(start) :: Nil, DocumentWrapper(wrapPage))
+}
