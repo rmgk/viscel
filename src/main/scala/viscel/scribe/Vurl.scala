c9e71312893a3a5246eb7dee9c1ee5f8e2874dde
ragnar <ragnar@rmgk.de>
2018-08-19 11:40:54 +0200

    update documentation

--- src/main/scala/viscel/scribe/Vurl.scala
+++ src/main/scala/viscel/scribe/Vurl.scala
@@ -11,7 +11,7 @@ import scala.language.implicitConversions
 
 /** Abstraction over possible representation for URLs.
   * Url handling is more complicated than it looks, we currently represent them internally as [[akka.http.scaladsl.model.Uri]],
-  * mostly because [[viscel.crawl.RequestUtil]] can directly use them.
+  * mostly because [[viscel.crawl.WebRequestInterface]] can directly use them.
   * Note that we do use [[java.net.URL]] to parse strings and then convert them manually.
   * Yes, this is because [[akka.http.scaladsl.model.Uri]] string parsing did not always work as expected,
   * and [[java.net.URL]] is more stable. */
