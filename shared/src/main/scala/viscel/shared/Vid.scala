b876efc1182b0f2c7517e7b24cf8e10c14c0c7b8
ragnar <ragnar@rmgk.de>
2018-08-26 19:41:07 +0200

    display Vid as string

--- shared/src/main/scala/viscel/shared/Vid.scala
+++ shared/src/main/scala/viscel/shared/Vid.scala
@@ -4,7 +4,9 @@ import io.circe.{Decoder, Encoder, KeyDecoder, KeyEncoder}
 
 import scala.util.matching.Regex
 
-final class Vid private(val str: String) extends AnyVal
+final class Vid private(val str: String) extends AnyVal {
+  override def toString: String = str
+}
 
 object Vid {
   val idregex: Regex = """^[\w-]+$""".r
