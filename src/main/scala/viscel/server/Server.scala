c500f7e276276969a88f3febbcd9fcd603725bf4
ragnar <ragnar@rmgk.de>
2019-08-24 16:27:24 +0200

    fix jsnames

--- src/main/scala/viscel/server/Server.scala
+++ src/main/scala/viscel/server/Server.scala
@@ -38,9 +38,9 @@ class Server(userStore: Users,
     path(name){getFromFile(staticPath.resolve(file).toFile)}
 
   val staticRoute: Route =
-    stc("js", "viscel-web-fastopt.js.gz") ~
+    stc("js", "app-fastopt.js.gz") ~
     stc("css", "style.css.gz") ~
-    stc("viscel-web-fastopt.js.map", "viscel-web-fastopt.js.map.gz") ~
+    stc("app-fastopt.js.map", "app-fastopt.js.map.gz") ~
     stc("style.css.map", "style.css.map.gz")
 
 
