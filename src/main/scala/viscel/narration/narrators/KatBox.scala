defb28283f2e2351fbcdf3414ff8240d2e01c1e3
ragnar <ragnar@rmgk.de>
2019-09-27 18:07:26 +0200

    add katbox rescues

--- src/main/scala/viscel/narration/narrators/KatBox.scala
+++ src/main/scala/viscel/narration/narrators/KatBox.scala
@@ -13,28 +13,18 @@ import scala.collection.immutable.Set
 object KatBox {
 
   val cores: Set[Narrator] = Set[(String, String, Option[Vurl])](
-    ("addictivescience", "Addictive Science", None),
-    ("ai", "Artificial Incident", None),
-    ("cblue", "Caribbean Blue", None),
     ("desertfox", "Desert Fox", None),
     ("dmfa", "DMFA", None),
     ("draconia", "Draconia Chronicles", None),
     //("falsestart", "False Start", None), // requires age check
     ("iba", "Itsy Bitsy Adventures", None),
-    ("imew", "iMew", None),
-    ("laslindas", "Las Lindas!", Some[Vurl]("http://laslindas.katbox.net/las-lindas/")),
     ("rixie", "Debunkers", None),
     ("oasis", "Oasis", None),
     ("ourworld", "Our World", None),
-    ("paprika", "Paprika", None),
     ("peterandcompany", "Peter and Company", None),
     ("peterandwhitney", "Peter and Whitney", None),
     ("pmp", "Practice Makes Perfect", None),
-    ("rascals", "Rascals", Some[Vurl]("http://rascals.katbox.net/rascals/")),
-    ("theeye", "The Eye of Ramalach", None),
-    ("tinaofthesouth", "Tina of the South", None),
     ("uberquest", "Uber Quest", None),
-    ("yosh", "Yosh!", None),
   ).map { case (_id, _name, _url) =>
     NarratorADT(Vid.from(s"KatBox_${_id}"), _name,
                 List(DataRow.Link(_url.getOrElse(s"http://${_id}.katbox.net/archive/"), List(Volatile.toString))),
