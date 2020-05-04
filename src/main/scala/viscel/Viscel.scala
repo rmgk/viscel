038f1a3a4f84d4aa199e50fa76e672e9ef9b7084
ragnar <ragnar@rmgk.de>
2020-05-04 21:19:04 +0200

    less option repetition

--- src/main/scala/viscel/Viscel.scala
+++ src/main/scala/viscel/Viscel.scala
@@ -9,37 +9,24 @@ import viscel.shared.Log
 
 object Viscel {
 
-  val port       : Opts[Int]     = Opts.option[Int](long = "port",
-                                                    help = "Weberver listening port.")
-                                   .withDefault(2358)
-  val interface  : Opts[String]  = Opts.option[String](long = "interface", metavar = "interface",
-                                                       help = "Interface to bind the server on.")
-                                   .withDefault("0")
-  val server     : Opts[Boolean] = Opts.flag(long = "noserver",
-                                             help = "Do not start the server.").orTrue
-  val core       : Opts[Boolean] = Opts.flag(long = "nodownload",
-                                             help = "Do not start the downloader.").orTrue
-  val optBasedir : Opts[Path]    = Opts.option[Path](long = "basedir", metavar = "directory",
-                                                     help = "Base directory to store settings and data.")
-                                   .withDefault(Paths.get("./data/"))
-  val optStatic  : Opts[Path]    = Opts.option[Path](long = "static", metavar = "directory",
-                                                     help = "Directory of static resources.")
-                                   .withDefault(Paths.get("./static/"))
-  val shutdown   : Opts[Boolean] = Opts.flag(long = "shutdown",
-                                             help = "Shutdown directly.")
-                                   .orFalse
-  val cleanblobs : Opts[Boolean] = Opts.flag(long = "cleanblobs",
-                                             help = "Cleans blobs from blobstore which are no longer linked.")
-                                   .orFalse
-  val optBlobdir : Opts[Path]    = Opts.option[Path](long = "blobdir", metavar = "directory",
-                                                     help = "Directory to store blobs (the images). Can be absolute, otherwise relative to basedir.")
-                                   .withDefault(Paths.get("./blobs/"))
-  val optPostsdir: Opts[Path]    = Opts.option[Path](long = "postsdir", metavar = "directory",
-                                                     help = "Directory to store posts. Can be absolute, otherwise relative to basedir.")
-                                   .withDefault(Paths.get("./posts/"))
+  val args =
+    (Opts.option[Path](long = "basedir", metavar = "directory", help = "Base directory to store settings and data.")
+         .withDefault(Paths.get("./data/")),
+      Opts.option[Int](long = "port", help = "Weberver listening port.").withDefault(2358),
+      Opts.option[String](long = "interface", metavar = "interface", help = "Interface to bind the server on.")
+          .withDefault("0"),
+      Opts.flag(long = "noserver", help = "Do not start the server.").orTrue,
+      Opts.flag(long = "nodownload", help = "Do not start the downloader.").orTrue,
+      Opts.flag(long = "cleanblobs", help = "Cleans blobs from blobstore which are no longer linked.").orFalse,
+      Opts.option[Path](long = "blobdir", metavar = "directory",
+                        help = "Directory to store blobs (the images). Can be absolute, otherwise relative to basedir.")
+          .withDefault(Paths.get("./blobs/")),
+      Opts.flag(long = "shutdown", help = "Shutdown directly.").orFalse,
+      Opts.option[Path](long = "static", metavar = "directory", help = "Directory of static resources.")
+          .withDefault(Paths.get("./static/")))
 
   val command: Command[Services] = Command(name = "viscel", header = "Start viscel!") {
-    (optBasedir, port, interface, server, core, cleanblobs, optBlobdir, shutdown, optStatic).mapN {
+    args.mapN {
       (optBasedir, port, interface, server, core, cleanblobs, optBlobdir, shutdown, optStatic) =>
 
         val staticCandidates = List(File(optBasedir.resolve(optStatic)),
@@ -47,11 +34,11 @@ object Viscel {
 
         val staticDir =
           staticCandidates.find(_.isDirectory)
-          .getOrElse {
-            println(s"Missing static resource directory, " +
-                    s"tried ${staticCandidates.map(c => s"»$c«").mkString(", ")}.")
-            sys.exit(0)
-          }
+                          .getOrElse {
+                            println(s"Missing static resource directory, " +
+                                    s"tried ${staticCandidates.map(c => s"»$c«").mkString(", ")}.")
+                            sys.exit(0)
+                          }
 
         val services = new Services(optBasedir, optBlobdir, staticDir.path, interface, port)
 
