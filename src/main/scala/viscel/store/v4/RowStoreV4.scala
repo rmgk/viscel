b25e0c598ccc05fcdb9b1ddc39f538830745566c
ragnar <ragnar@rmgk.de>
2020-05-25 21:55:40 +0200

    formatting

--- src/main/scala/viscel/store/v4/RowStoreV4.scala
+++ src/main/scala/viscel/store/v4/RowStoreV4.scala
@@ -11,48 +11,48 @@ import viscel.shared.Vid
 import viscel.store.CirceStorage._
 import viscel.store.{Book, CirceStorage}
 
-class RowStoreV4 (db4dir: Path) {
+class RowStoreV4(db4dir: Path) {
 
-    val base = File(db4dir)
+  val base = File(db4dir)
 
 
   def allVids(): List[Vid] = synchronized {
     base.list(_.isRegularFile, 1).map(f => Vid.from(f.name)).toList
   }
 
-    def open(narrator: Narrator): RowAppender = synchronized {
-      open(narrator.id, narrator.name)
+  def open(narrator: Narrator): RowAppender = synchronized {
+    open(narrator.id, narrator.name)
+  }
+
+  def open(id: Vid, name: String): RowAppender = synchronized {
+    val f = base / id.str
+    if (!f.exists || f.size <= 0) CirceStorage.store(f.path, name)
+    new RowAppender(f)
+  }
+
+
+  def load(id: Vid): (String, List[DataRow]) = synchronized {
+    Log.info(s"loading $id")
+
+    val f = base / id.str
+
+    if (!f.isRegularFile || f.size == 0)
+      throw new IllegalStateException(s"$f does not contain data")
+    else {
+      val lines = f.lineIterator(StandardCharsets.UTF_8)
+      val name  = io.circe.parser.decode[String](lines.next()).toTry.get
+
+      val dataRows = lines.zipWithIndex.map { case (line, nr) =>
+        io.circe.parser.decode[DataRow](line) match {
+          case Right(s) => s
+          case Left(t)  =>
+            Log.error(s"Failed to decode $id:${nr + 2}: $line")
+            throw t
+        }
+      }.toList
+      (name, dataRows)
     }
-
-    def open(id: Vid, name: String): RowAppender = synchronized{
-      val f = base / id.str
-      if (!f.exists || f.size <= 0) CirceStorage.store(f.path, name)
-      new RowAppender(f)
-    }
-
-
-    def load(id: Vid): (String, List[DataRow]) = synchronized {
-      Log.info(s"loading $id")
-
-      val f = base / id.str
-
-      if (!f.isRegularFile || f.size == 0)
-        throw new IllegalStateException(s"$f does not contain data")
-      else {
-        val lines = f.lineIterator(StandardCharsets.UTF_8)
-        val name = io.circe.parser.decode[String](lines.next()).toTry.get
-
-        val dataRows = lines.zipWithIndex.map { case (line, nr) =>
-          io.circe.parser.decode[DataRow](line) match {
-            case Right(s) => s
-            case Left(t)  =>
-              Log.error(s"Failed to decode $id:${nr + 2}: $line")
-              throw t
-          }
-        }.toList
-        (name, dataRows)
-      }
-    }
+  }
 
   def loadBook(id: Vid): Book = {
     val (name, rows) = try {load(id)} catch {
