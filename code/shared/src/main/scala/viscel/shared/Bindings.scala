683ee6ba2fcd0d481badb58b1dc11cf5de40c8d8
ragnar <ragnar@rmgk.de>
2020-06-26 15:31:08 +0200

    workaround Scala warning in some generated code

--- code/shared/src/main/scala/viscel/shared/Bindings.scala
+++ code/shared/src/main/scala/viscel/shared/Bindings.scala
@@ -2,9 +2,10 @@ package viscel.shared
 
 import loci.registry.Binding
 import loci.transmitter.IdenticallyTransmittable
-
 import viscel.shared.JsoniterLociSerializable._
 import viscel.shared.JsoniterCodecs._
+
+import scala.annotation.nowarn
 
 object Bindings {
   type IT[V] = IdenticallyTransmittable[V]
@@ -15,13 +16,14 @@ object Bindings {
   implicit val _Tb: IT[Bookmark] = IdenticallyTransmittable()
   implicit val _Tbm: IT[Map[Vid, Bookmark]] = IdenticallyTransmittable()
 
+  @nowarn
   val descriptions = Binding[() => Map[Vid, Description]]("descriptions")
-
+  @nowarn
   val contents = Binding[Vid => Option[Contents]]("contents")
-
+  @nowarn
   val hint = Binding[(Vid, Boolean) => Unit]("hint")
-
+  @nowarn
   val bookmarksMapBindig = Binding[Map[Vid, Bookmark] => Unit]("bookmarksmap")
-
+  @nowarn
   val version = Binding[String]("version")
 }
