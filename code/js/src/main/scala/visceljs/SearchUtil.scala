947cf0de9069c27c7f78d161717d8605d54bfc13
ragnar <ragnar@rmgk.de>
2020-06-26 13:07:34 +0200

    sligthly optimize fuzzy match

--- code/js/src/main/scala/visceljs/SearchUtil.scala
+++ code/js/src/main/scala/visceljs/SearchUtil.scala
@@ -6,23 +6,33 @@ import scala.annotation.tailrec
 
 object SearchUtil {
   def search[T](query: String, items: List[(String, T)]): List[T] = {
-    val lcql = query.toLowerCase.replaceAll("""\s+""", "").toList
+    val lcql = query.toLowerCase.replaceAll("""\s+""", "")
     if (lcql.isEmpty) items.map(_._2)
     else items
-      .map { item => item -> fuzzyMatch(lcql, item._1.toLowerCase.toList) }
+      .view
+      .map { item => item -> fuzzyMatch(lcql, item._1.toLowerCase) }
       .filter {_._2 > 0}
+      .toList
       .sortBy {-_._2}
       .map {_._1._2}
   }
 
-  @tailrec
-  def fuzzyMatch(query: List[Char], text: List[Char], score: Long = 0, bestScore: Long = 0): Long = query match {
-    case Nil => bestScore + score * score
-    case q :: qs => text match {
-      case Nil => 0
-      case t :: ts =>
-        if (t == q) fuzzyMatch(qs, ts, score + 1, bestScore)
-        else fuzzyMatch(query, ts, 0, score * score + bestScore)
+  def fuzzyMatch(query: String, text: String): Long = {
+    @tailrec
+    def rec(score: Long, bestScore: Long, qpos: Int, tpos: Int): Long = {
+      if (query.length <= qpos) {
+        bestScore + score * score
+      }
+      else {
+        if (text.length <= tpos) {
+          0
+        }
+        else {
+          if (text(tpos) == query(qpos)) rec(score + 1, bestScore, qpos + 1, tpos + 1)
+          else rec(0, score * score + bestScore, qpos, tpos + 1)
+        }
+      }
     }
+    rec(0, 0, 0, 0)
   }
 }
