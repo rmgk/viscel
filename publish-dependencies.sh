e5af39c2dea1ee71fea2d7f4c6e0481ec6440efe
ragnar <ragnar@rmgk.de>
2020-10-11 23:39:25 +0200

    i am so bad at this

--- publish-dependencies.sh
+++ publish-dependencies.sh
@@ -2,14 +2,13 @@
 
 mkdir dependencies
 cd dependencies
-mkdir rescala
+git clone https://github.com/rescala-lang/REScala.git rescala
 cd rescala
-git clone https://github.com/rescala-lang/REScala.git
 git reset --hard c3c37c4739b4a0b0ab40adc5c94473d0f8ccc498
 sbt publishLocal
 cd ..
-mkdir loci
-git clone https://github.com/scala-loci/scala-loci.git
+git clone https://github.com/scala-loci/scala-loci.git loci
+cd loci
 git reset --hard a880ec8639ff48f6d85a869cb8a3117dfa06287a
 sbt publishLocal
 cd ..
