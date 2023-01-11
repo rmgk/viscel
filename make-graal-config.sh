d02427c90527062b88e0ed6494502ca12e9aca2a
ragnar <git@rmgk.de>
2023-01-11 20:09:23 +0100

    bundle before running

--- make-graal-config.sh
+++ make-graal-config.sh
@@ -2,7 +2,7 @@
 
 rm -r jvm/src/main/resources/META-INF/native-image/generated/
 
-sbtn stageJars
+sbtn stageJars; vbundle
 
 cs java --jvm graalvm-java17:22.3.0 -agentlib:native-image-agent=config-output-dir=jvm/src/main/resources/META-INF/native-image/generated --class-path "jvm/target/jars/*" viscel.Viscel --static target/resources/static/ --basedir target/base
 
