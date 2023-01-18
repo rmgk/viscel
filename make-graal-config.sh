2c2c658bff65aa3d26c9277cb767666ce6ab98ff
ragnar <git@rmgk.de>
2023-01-18 23:26:11 +0100

    still does not work as native image :/

--- make-graal-config.sh
+++ make-graal-config.sh
@@ -2,6 +2,6 @@
 
 rm -r jvm/src/main/resources/META-INF/native-image/generated/
 
-sbtn stageJars; vbundle
+sbtn "reload; stageJars; vbundle"
 
 cs java --jvm graalvm-java19:22.3.0 -agentlib:native-image-agent=config-output-dir=jvm/src/main/resources/META-INF/native-image/generated --class-path "jvm/target/jars/*" viscel.Viscel --static target/resources/static/ --basedir target/base
--- make-native-image.sh
+++ make-native-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 sbtn stageJars
-native-image --class-path "jvm/target/jars/*" viscel.Viscel
+native-image --enable-preview --class-path "jvm/target/jars/*" viscel.Viscel
