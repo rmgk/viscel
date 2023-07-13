a13fa3ac9528620a75450461a7149bc47ce93127
ragnar <git@rmgk.de>
2023-07-13 13:11:55 +0200

    server too old 😂

--- make-native-image.sh
+++ make-native-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 sbt --client stageJars
-native-image --enable-preview --class-path "jvm/target/jars/*" viscel.Viscel
+native-image --enable-preview -march=x86-64-v2 --class-path "jvm/target/jars/*" viscel.Viscel
