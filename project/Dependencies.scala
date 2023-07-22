bc7e8587dc9e06ba7e87b284f1e5d3410d27a03e
ragnar <git@rmgk.de>
2023-07-22 12:09:31 +0200

    move versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,42 +7,34 @@ import sbt.Keys.libraryDependencies
 
 object Dependencies {
 
-  object Versions {
-    val scala211      = "2.11.12"
-    val scala212      = "2.12.17"
-    val scala213      = "2.13.10"
-    val scala3        = "3.3.0"
-  }
-
-  import Dependencies.Versions as V
-
-  def directories   = libraryDependencies += "dev.dirs" % "directories" % "26"
-  val jol           = libraryDependencies += "org.openjdk.jol" % "jol-core" % "0.17"
-  val jsoniterScala = libraryDependencies += "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.23.2"
-  val jsoup         = libraryDependencies += "org.jsoup" % "jsoup" % "1.16.1"
-  val munit         = libraryDependencies += "org.scalameta" %%% "munit" % "1.0.0-M8" % Test
-  val munitCheck    = libraryDependencies += "org.scalameta" %%% "munit-scalacheck" % "1.0.0-M8" % Test
-  val okHttp        = libraryDependencies += "com.squareup.okhttp3" % "okhttp" % "4.10.0"
-  val pprint        = libraryDependencies += "com.lihaoyi" %%% "pprint" % "0.8.0"
-  val quicklens     = libraryDependencies += "com.softwaremill.quicklens" %%% "quicklens" % "1.9.0"
-  val scalacheck    = libraryDependencies += "org.scalacheck" %%% "scalacheck" % "1.17.0" % Test
-  val scalaJavaTime = libraryDependencies += "io.github.cquiroz" %%% "scala-java-time" % "2.3.0"
-  val scalajsDom    = libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "2.6.0"
-  val scalatags     = libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.12.0"
-  val scribe        = libraryDependencies += "com.outr" %%% "scribe" % "3.10.7"
-  val scribeSlf4j   = libraryDependencies += "com.outr" %% "scribe-slf4j" % "3.10.7"
-  val scribeSlf4j2  = libraryDependencies += "com.outr" %% "scribe-slf4j2" % "3.10.7"
-  val sourcecode    = libraryDependencies += "com.lihaoyi" %%% "sourcecode" % "0.3.0"
-  val sqliteJdbc    = libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.42.0.0"
-  val upickle       = libraryDependencies += "com.lihaoyi" %%% "upickle" % "3.1.2"
+  def directories = libraryDependencies += "dev.dirs"        % "directories" % "26"
+  val jol         = libraryDependencies += "org.openjdk.jol" % "jol-core"    % "0.17"
+  val jsoniterScala =
+    libraryDependencies += "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.23.2"
+  val jsoup         = libraryDependencies += "org.jsoup"                    % "jsoup"            % "1.16.1"
+  val munit         = libraryDependencies += "org.scalameta"              %%% "munit"            % "1.0.0-M8" % Test
+  val munitCheck    = libraryDependencies += "org.scalameta"              %%% "munit-scalacheck" % "1.0.0-M8" % Test
+  val okHttp        = libraryDependencies += "com.squareup.okhttp3"         % "okhttp"           % "4.10.0"
+  val pprint        = libraryDependencies += "com.lihaoyi"                %%% "pprint"           % "0.8.0"
+  val quicklens     = libraryDependencies += "com.softwaremill.quicklens" %%% "quicklens"        % "1.9.0"
+  val scalacheck    = libraryDependencies += "org.scalacheck"             %%% "scalacheck"       % "1.17.0"   % Test
+  val scalaJavaTime = libraryDependencies += "io.github.cquiroz"          %%% "scala-java-time"  % "2.3.0"
+  val scalajsDom    = libraryDependencies += "org.scala-js"               %%% "scalajs-dom"      % "2.6.0"
+  val scalatags     = libraryDependencies += "com.lihaoyi"                %%% "scalatags"        % "0.12.0"
+  val scribe        = libraryDependencies += "com.outr"                   %%% "scribe"           % "3.10.7"
+  val scribeSlf4j   = libraryDependencies += "com.outr"                    %% "scribe-slf4j"     % "3.10.7"
+  val scribeSlf4j2  = libraryDependencies += "com.outr"                    %% "scribe-slf4j2"    % "3.10.7"
+  val sourcecode    = libraryDependencies += "com.lihaoyi"                %%% "sourcecode"       % "0.3.0"
+  val sqliteJdbc    = libraryDependencies += "org.xerial"                   % "sqlite-jdbc"      % "3.42.0.0"
+  val upickle       = libraryDependencies += "com.lihaoyi"                %%% "upickle"          % "3.1.2"
 
   object slips {
-    val chain   = libraryDependencies +=  "de.rmgk.slips" %%% "chain" % "0.5.0"
-    val delay   = libraryDependencies +=  "de.rmgk.slips" %%% "delay" % "0.5.0"
-    val logging = libraryDependencies +=  "de.rmgk.slips" %%% "logging" % "0.5.0"
-    val options = libraryDependencies +=  "de.rmgk.slips" %%% "options" % "0.7.0"
-    val scip    = libraryDependencies +=  "de.rmgk.slips" %%% "scip" % "0.5.0"
-    val script  = libraryDependencies +=  "de.rmgk.slips" %%% "script" % "0.7.0"
+    val chain   = libraryDependencies += "de.rmgk.slips" %%% "chain"   % "0.5.0"
+    val delay   = libraryDependencies += "de.rmgk.slips" %%% "delay"   % "0.5.0"
+    val logging = libraryDependencies += "de.rmgk.slips" %%% "logging" % "0.5.0"
+    val options = libraryDependencies += "de.rmgk.slips" %%% "options" % "0.7.0"
+    val scip    = libraryDependencies += "de.rmgk.slips" %%% "scip"    % "0.5.0"
+    val script  = libraryDependencies += "de.rmgk.slips" %%% "script"  % "0.7.0"
   }
 
   object loci {
